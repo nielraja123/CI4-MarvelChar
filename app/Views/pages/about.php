9bf73b96b68643beaae2a6cf29052ff1841090d5
ThinkPad <nielraja123@gmail.com>
2023-04-23 15:06:04 +0700

    Make Home and About Me Pages

---
+++ app/Views/pages/about.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+            <h1>About Me</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis modi impedit necessitatibus quae quidem officia illo a debitis et, itaque porro velit quisquam laboriosam libero, temporibus optio minus quam cumque?</p>
+        </div>
+    </div>
+</div>
